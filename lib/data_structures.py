7bf1fdee13338947cc924a0aed8b3c72c0ecbc5d
Akanle Jeremy <akanlejeremy@gmail.com>
2024-11-26 10:53:58 +0300

    data structures passing

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,28 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return[d.get("name") for d in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return[d for d in spicy_foods if d.get("heat_level") > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
-
+    for d in spicy_foods:
+        print(f'{d["name"]} ({d["cuisine"]}) | Heat Level: {"🌶" * d["heat_level"]}')
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for d in spicy_foods:
+        if d["cuisine"] == cuisine:
+            return d
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    return print_spicy_foods(get_spiciest_foods(spicy_foods))
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total = 0
+    for d in spicy_foods:
+        total+=d["heat_level"]
+    return total / len(spicy_foods)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
